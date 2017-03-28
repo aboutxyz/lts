60e4b0e6c931bc673190bd24ccb4cf19fb9ca677
aboutxyz <haishangmuxucao@dlmu.edu.cn>
2017-03-28 15:56:00 +0800

    echarts

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 # coding:utf-8
 import re
 import pymongo
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
 
 from bson.son import SON
 connection = pymongo.MongoClient("localhost", 27017)
@@ -8,7 +11,17 @@ db = connection.ltsdata
 collection = db.data
 
 pipeline = [{"$group": {"_id" :{"client" : "$订舱人名称", "voyage":"$航线"} , "count": {"$sum": "$TEU"}}},{"$sort": SON([("count", -1), ("_id.voyage", -1)])}]
-print list(collection.aggregate(pipeline))
+
+
+@app.route('/')
+def index():
+    # return str(list(collection.aggregate(pipeline))[0]['_id'])
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
+
+
         
 # db.data.aggregate([ { $group: {"_id": { "client" : "$订舱人名称", "voyage":"$航线"} , "number":{$sum:"$TEU"}} } ,{$sort:{"_id.voyage":1, number:-1}}]); 
 
